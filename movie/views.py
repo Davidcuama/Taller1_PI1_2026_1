206c6b6dee0c84e6d66119713a572670306a021b
David Cuadros <davidacua12@gmail.com>
2026-02-05 11:24:16 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'David'})
+    return render(request, 'home.html', {'name': 'David Cuadros'})
 
 def about(request):
     return render(request, 'about.html')
